86ec0312cca930f9e2b9d321ed9887172ab8c790
Malodeporet <malo-94@hotmail.fr>
2021-07-14 15:13:44 +0200

    final

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-container">
+                <p>Projet réalisé par Malo de Poret - 2021</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
